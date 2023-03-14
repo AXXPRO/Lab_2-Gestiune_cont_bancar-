71f0a2350f7a2a0f7b5ec7236822e5694c07f38f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-14 22:53:20 +0200

    Added tests for repo and service

--- Lab_2/Teste/teste.c
+++ Lab_2/Teste/teste.c
@@ -5,6 +5,7 @@
 
 void teste_domain();
 void teste_repository();
+void teste_service();
 /*
     Functia de rulare a tuturor testelor
 */
@@ -14,7 +15,42 @@ void run_all_tests()
     printf("Teste domain trecute!\n");
     teste_repository();
     printf("Teste repository trecute!\n");
+    teste_service();
+    printf("Teste service trecute!\n");
 
+
+}
+/*
+Teste pentru functiile legate de service
+*/
+void teste_service()
+{       
+    int id_1 = 0;
+    int suma_1  =50;
+    int ziua_1 = 23;
+    enum tip tip_1 = intrare;
+    char* descriere = (char*)malloc(sizeof(char));
+    strcpy(descriere, "Lemne");
+
+    lista* lista_test = creaza_lista();
+
+
+    adaugare_service(id_1,  suma_1, ziua_1,  tip_1, descriere, lista_test);
+
+    assert(get_tranzactie(lista_test, 0) != (Tranzactie*)NULL);
+
+   assert(modificare_service(1,suma_1+1, ziua_1,  tip_1, descriere, lista_test) == 0 );
+    assert(modificare_service(0,suma_1+1, ziua_1,  tip_1, descriere, lista_test) == 1 );
+
+    Tranzactie* tranz_modificata = get_tranzactie(lista_test, 0);
+    assert(get_suma(tranz_modificata)==51);
+
+    assert(sterge_tranzactie(lista_test,2) == 0);
+    assert(sterge_tranzactie(lista_test,0) == 1);
+  tranz_modificata = get_tranzactie(lista_test, 0);
+    assert(tranz_modificata == (Tranzactie*)NULL);
+   
+    free(lista_test);
 
 }
 
@@ -60,6 +96,19 @@ void teste_repository()
 
     tranz_noua = creaza_tranzactie(2,900,30,iesire,d4);
     modificare_tranzactie(l, tranz_noua, get_id(tranz3));
+
+    Tranzactie* tranzactie_cautata_invalida = get_tranzactie(l,7);
+    assert(tranzactie_cautata_invalida == (Tranzactie*)NULL);
+
+    Tranzactie* tranzactie_cautata = get_tranzactie(l,0);
+    assert(tranzactie_cautata ==tranz1);
+
+
+    sterge_tranzactie(l, 2);
+    tranzactie_cautata_invalida =get_tranzactie(l,2);
+    assert(tranzactie_cautata_invalida == (Tranzactie*)NULL);
+
+    assert(numar_elemente(l) == 2);
     distruge(l);
 
     
@@ -98,6 +147,15 @@ void teste_domain()
     assert(suma_nou==get_suma(tranz));
     assert(ziua_nou==get_ziua(tranz));
     assert(strcmp(descriere_nou, get_descriere(tranz))==0);
+    
+    char* string_tranz = (char*)malloc(sizeof(char));
+
+   string_tranz = string_tranzactie(tranz);
+   // printf("%s\n", string_tranz);
+    assert(strcmp(string_tranz,"Tranzactia cu id-ul 1 si suma 100 din ziua 9 de tipul iesire are descrierea cerere" ) == 0);
+
+   
+    free(string_tranz);
     free(tranz);
 
 }
--- Lab_2/main.c
+++ Lab_2/main.c
@@ -3,13 +3,12 @@
 #include <string.h>
 //#include "C:\Users\Gabi\Desktop\Lab_2\Repository\domain.h"
 #include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Repository\domain.c"
-#include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Teste\teste.c"
 #include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\UI\ui.c"
 #include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Repository\repo.c"
 #include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Service\service.c"
 #include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Validation\valid.c"
 
-
+#include "C:\Users\Gabi\Desktop\Lab_2-Gestiune_cont_bancar-\Lab_2\Teste\teste.c"
 int main()
 {   run_all_tests();
     run_ui();
